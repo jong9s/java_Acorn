261405d786fde448d5235567a7b615c455f468e4
jong9 <jonghunseo@naver.com>
2025-06-16 16:34:44 +0900

    Step01_DataType 프로젝트 추가함

---
+++ Step01_DataType/src/test/main/MainClass03.java
@@ -0,0 +1,22 @@
+package test.main;
+
+public class MainClass03 {
+
+	public static void main(String[] args) {
+		// 논리형 변수 선언과 동시에 값 대입하기
+		boolean isRun = false;
+
+		if (isRun) {
+			System.out.println("달려요");
+		}
+		// 비교 연산의 결과로 얻어진 boolean 값 담기
+		boolean isGreater = 10 > 1;
+		// 논리 연산의 결과로 얻어진 boolean 값 담기
+		boolean result = true || false;
+		// 한 번 선언한 변수는 다시 선언할 수 없다.
+		// boolean result = false;
+		result = false; // 변수에 다른 값을 대입할 수 있다(동일한 type 인 경우)
+
+	}
+
+}
