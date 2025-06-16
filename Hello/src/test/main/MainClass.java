1ab3bbb5de902efa3e28769f34f788ae23c64a3c
jong9 <jonghunseo@naver.com>
2025-06-16 16:07:43 +0900

    Hello 프로젝트 추가함

---
+++ Hello/src/test/main/MainClass.java
@@ -0,0 +1,22 @@
+package test.main;
+
+public class MainClass {
+	
+	// main 이라는 이름의 메소드를 클래스 안에 만들기
+	public static void main(String[] args) {
+		// main() 메소드 안에 있는 코드를 실행하려면 run 버튼을 누르면 된다
+		System.out.println("Hello, World!");
+		System.out.println("서종훈");
+		System.out.println("에이콘 아카데미");
+		
+		int num = 10;
+		
+		double num2 = 10.2;
+		num2 = 10.2002;
+		
+		boolean isRun = true;
+		isRun = false;
+		
+		String myName = "서종훈";
+	}
+}
