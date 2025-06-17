a325667a84e910b1293b51c551a5ca97e0afcfaa
jong9 <jonghunseo@naver.com>
2025-06-17 14:39:06 +0900

    MainClass5,6 추가

--- Step01_DataType/src/test/main/MainClass05.java
+++ Step01_DataType/src/test/main/MainClass05.java
@@ -23,7 +23,7 @@ public class MainClass05 {
 		double num2 = 10.2;
 		boolean isRun = true;
 		// 변수 안에 참조값이 들어가는 참조 데이터 type 인 String
-		String yourName = "빵빵이";
+		String yourName = "김옥지";
 		
 	}
 }
---
+++ Step01_DataType/src/test/main/MainClass06.java
@@ -0,0 +1,16 @@
+package test.main;
+
+public class MainClass06 {
+	public static void main(String[] args) {
+		// String type 의 기능(method) 사용해보기
+		
+		String str = "abcd1234";
+		// 문자열의 길이를 리턴하는 length() 메소드 사용해보기
+		int length = str.length();
+		// 문자열을 대문자로 변경해서 리턴하는 메소드 사용해보기
+		String upper = str.toUpperCase();
+		// 특정 문자열이 포함되어 있는지 여부를 리턴하는 메소드 사용해보기
+		boolean contains = str.contains("abc");
+		
+ 	}
+}
