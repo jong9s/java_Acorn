e521379b4f774f0765ed38b818da93d20080000b
jong9 <jonghunseo@naver.com>
2025-06-30 16:55:18 +0900

    Step12_Exception 추가

---
+++ Step12_Exception/src/test/main/MainClass05.java
@@ -0,0 +1,15 @@
+package test.main;
+/*
+ *  run 버튼을 누르면
+ */
+
+public class MainClass05 {
+	public static void main(String[] args) {
+		System.out.println("main 메소드가 시작 되었습니다.");
+		
+		// Exception 을 try~catch 로 묶지 않으면 예외처리의 주체는 메소드를 호출한 곳에서 처리를 한다
+//		Thread.sleep(5000);
+
+		System.out.println("main 메소드가 종료 됩니다.");
+	}
+}
